02c6eab2528bb59ac4b1d1cd5c5f82cd2a23da48
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-03-21 18:24:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,23 +13,20 @@ def is_enabled(value, default):
     elif value.strip().lower() in ["off", "false", "no", "0", "disable", "n"]: return False
     else: return default
 
-# basic information
 API_ID = int(os.environ.get('API_ID', '8914119'))
 API_HASH = os.environ.get('API_HASH', '652bae601b07c928b811bdb310fdb4b0')
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '7035412485:AAHRycb4xBCpemEtAfMjhjR0k7ONzKS-4tY')
 PORT = os.environ.get("PORT", "8080")
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1342641151').split()]
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002092375161'))
 
-# for force subscribe[Your Force Sub Channel Username Without @ (eg:- sd_bots)]
 F_SUB = os.environ.get("FORCE_SUB", "sd_bots") 
 
 S_GROUP = environ.get('S_GROUP', "https://t.me/sdbots_support")
 S_CHANNEL = environ.get('S_CHANNEL', "https://t.me/sd_bots")
 
-# for mongodb
-DATABASE_NAME = os.environ.get("DB_NAME", "")     
-DATABASE_URI  = os.environ.get("DB_URL", "")
+DATABASE_NAME = os.environ.get("DB_NAME", "sakurabis11")     
+DATABASE_URI  = os.environ.get("DB_URL", "mongodb+srv://sakurabis11:woljkA2QEGi03iYY@cluster0.w1oavmr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 MONGO_URL = os.environ.get('MONGO_URL', "")
