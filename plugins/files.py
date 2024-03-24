11e645b8a341e51e26e6979ac0c760b76c4aefed
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-03-24 10:36:53 +0530

    Delete plugins directory

--- plugins/files.py
+++
@@ -1,7 +0,0 @@
-import pyrogram
-from pyrogram import Client, filters
-from info import ADMINS
-
-@Client.on_message(filters.document & filters.private & filters.user(ADMINS))
-async def fileid(client, message):
-   await message.reply(message.document.file_id)
