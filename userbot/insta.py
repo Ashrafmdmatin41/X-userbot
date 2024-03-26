42712ef0234158181c1708ff43136880bdf56068
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-03-26 08:16:49 +0530

    Update insta.py

--- userbot/insta.py
+++ userbot/insta.py
@@ -18,7 +18,7 @@ headers = {
 }
 @Client.on_message(filters.regex(r'https?://.*instagram[^\s]+'))
 async def link_handler(client, message):
-    link = message.matches[0].group(0)
+    link = message.text
     global headers
     try:
         m = await message.reply_sticker("CAACAgUAAxkBAAITAmWEcdiJs9U2WtZXtWJlqVaI8diEAAIBAAPBJDExTOWVairA1m8eBA")
