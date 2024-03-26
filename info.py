1425d6ae5e56dd6615110fd99b2383bfe5e3fdab
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-03-27 00:37:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,3 +16,7 @@ def is_enabled(value, default):
 API_ID = int(os.environ.get('API_ID', '8914119'))
 API_HASH = os.environ.get('API_HASH', '652bae601b07c928b811bdb310fdb4b0')
 SESSION = os.environ.get('SESSION', 'BQCIBMcAYEOIDLlEo6JqLBX5_NAVMkxlQGx3tr13oyVj3c47yWYpnHUKx45hqCFWHOiWEBocQgANsFkMBu8G3MV8hlBJzDPSG5pvpA-sjJ7bPAb-anMsZapdU9DSyFHQgMTIWvTKZscTul2VO8s3icQqJrzcGx6cFEbnLGQyYg62uMHF2EoMP8NyLJWmxoOUvH-nNTLlaWMnQpuJyGHWZ0N1a4jclbpciUYS5Q9HyPE-ZKlDFERoxOMgkh7z_rmFzAzBfbLaPp3MmQbWEkDlTPD8H5okCqn9cNOTFjfxsDQ227z_cA3S5C3siXqjTxep0W5iBmgW9F-XUOGpfWXi4vtFcJE52AAAAABQBxP_AA')
+
+DATABASE_COLLECTION = "supergroups"
+DATABASE_NAME = os.environ.get("DB_NAME", "sakurabis11")     
+DATABASE_URI  = os.environ.get("DB_URL", "mongodb+srv://sakurabis11:woljkA2QEGi03iYY@cluster0.w1oavmr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
